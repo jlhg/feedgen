a604c035827b217e1109d9916d4b37110fbd459c
Jian-Long Huang <contact@jianlong.org>
2021-10-28 19:48:24 +0800

    Format code with gofmt

--- site/hackernews.go
+++ site/hackernews.go
@@ -1,103 +1,103 @@
 package site
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "regexp"
-    "strconv"
-    "strings"
-    "time"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
-    "github.com/gorilla/feeds"
+	"github.com/gorilla/feeds"
 
-    "github.com/jlhg/feedgen"
+	"github.com/jlhg/feedgen"
 )
 
 // HackernewsParser is a parser for Hacker News (https://news.ycombinator.com/).
-type HackernewsParser struct {}
+type HackernewsParser struct{}
 
 // GetFeed returns generated feed with the given query parameters.
 func (parser HackernewsParser) GetFeed(query feedgen.QueryValues) (feed *feeds.Feed, err error) {
-    category := query.Get("category")
-    switch category {
-    case "best":
-        feed, err = parser.GetBestFeed()
-    case "":
-        err = &feedgen.ParameterNotFoundError{"category"}
-    default:
-        err = &feedgen.ParameterValueInvalidError{"category"}
-        return
-    }
+	category := query.Get("category")
+	switch category {
+	case "best":
+		feed, err = parser.GetBestFeed()
+	case "":
+		err = &feedgen.ParameterNotFoundError{"category"}
+	default:
+		err = &feedgen.ParameterValueInvalidError{"category"}
+		return
+	}
 
-    return
+	return
 }
 
 // GetBestFeed returns generated feed with best category.
 func (parser HackernewsParser) GetBestFeed() (feed *feeds.Feed, err error) {
-    now := time.Now()
-    title := "Top Links | Hacker News"
-    url := "https://news.ycombinator.com/best"
-    feed = &feeds.Feed{
-        Title: title,
-        Link: &feeds.Link{Href: url},
-        Description: "",
-        Author: nil,
-        Created: now,
-    }
+	now := time.Now()
+	title := "Top Links | Hacker News"
+	url := "https://news.ycombinator.com/best"
+	feed = &feeds.Feed{
+		Title:       title,
+		Link:        &feeds.Link{Href: url},
+		Description: "",
+		Author:      nil,
+		Created:     now,
+	}
 
-    resp, err := http.Get(url)
-    if err != nil {
-        return
-    }
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
 
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return
-    }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
 
-    re := regexp.MustCompile(`(?s)<td class="title"><a href="(.+?)" class="(storylink|titlelink)">(.+?)</a>.+?<span class="score" id=".+?">(\d+?) points</span>.+?by <a href=".+?" class="hnuser">(.+?)</a>.+?(\d+?) (days?|hours?|minutes?) ago.+?<a href="(.+?)">.+?(\d+?)&nbsp;comments</a>`)
-    matchGroup := re.FindAllSubmatch(body, -1)
-    if len(matchGroup) == 0 {
-        err = &feedgen.ItemFetchError{url}
-        return
-    }
+	re := regexp.MustCompile(`(?s)<td class="title"><a href="(.+?)" class="(storylink|titlelink)">(.+?)</a>.+?<span class="score" id=".+?">(\d+?) points</span>.+?by <a href=".+?" class="hnuser">(.+?)</a>.+?(\d+?) (days?|hours?|minutes?) ago.+?<a href="(.+?)">.+?(\d+?)&nbsp;comments</a>`)
+	matchGroup := re.FindAllSubmatch(body, -1)
+	if len(matchGroup) == 0 {
+		err = &feedgen.ItemFetchError{url}
+		return
+	}
 
-    for _, m := range matchGroup {
-        itemLink := string(m[1])
-        itemTitle := string(m[3])
-        itemPoint := string(m[4])
-        itemAuthor := string(m[5])
-        itemBeforeTime := string(m[6])
-        itemBeforeTimeUnit := string(m[7])
-        itemCommentPath := string(m[8])
-        itemCommentCount := string(m[9])
-        itemCommentLink := fmt.Sprintf("https://news.ycombinator.com/%s", itemCommentPath)
-        itemDescription := fmt.Sprintf("%s points. <a href=\"%s\" >%s comments</a>", itemPoint, itemCommentLink, itemCommentCount)
-        created := now
+	for _, m := range matchGroup {
+		itemLink := string(m[1])
+		itemTitle := string(m[3])
+		itemPoint := string(m[4])
+		itemAuthor := string(m[5])
+		itemBeforeTime := string(m[6])
+		itemBeforeTimeUnit := string(m[7])
+		itemCommentPath := string(m[8])
+		itemCommentCount := string(m[9])
+		itemCommentLink := fmt.Sprintf("https://news.ycombinator.com/%s", itemCommentPath)
+		itemDescription := fmt.Sprintf("%s points. <a href=\"%s\" >%s comments</a>", itemPoint, itemCommentLink, itemCommentCount)
+		created := now
 
-        if strings.Contains(itemBeforeTimeUnit, "day") {
-            day, _ := strconv.Atoi(itemBeforeTime)
-            created = now.AddDate(0, 0, -day)
-        } else if strings.Contains(itemBeforeTimeUnit, "hour") {
-            duration, _ := time.ParseDuration(fmt.Sprintf("-%sh", itemBeforeTime))
-            created = now.Add(duration)
-        } else if strings.Contains(itemBeforeTimeUnit, "minute") {
-            duration, _ := time.ParseDuration(fmt.Sprintf("-%sm", itemBeforeTime))
-            created = now.Add(duration)
-        }
+		if strings.Contains(itemBeforeTimeUnit, "day") {
+			day, _ := strconv.Atoi(itemBeforeTime)
+			created = now.AddDate(0, 0, -day)
+		} else if strings.Contains(itemBeforeTimeUnit, "hour") {
+			duration, _ := time.ParseDuration(fmt.Sprintf("-%sh", itemBeforeTime))
+			created = now.Add(duration)
+		} else if strings.Contains(itemBeforeTimeUnit, "minute") {
+			duration, _ := time.ParseDuration(fmt.Sprintf("-%sm", itemBeforeTime))
+			created = now.Add(duration)
+		}
 
-        feed.Add(&feeds.Item{
-            Id: itemCommentLink,
-            Title: string(itemTitle),
-            Link: &feeds.Link{Href: itemLink},
-            Description: itemDescription,
-            Author: &feeds.Author{Name: itemAuthor},
-            Created: created,
-        })
-    }
+		feed.Add(&feeds.Item{
+			Id:          itemCommentLink,
+			Title:       string(itemTitle),
+			Link:        &feeds.Link{Href: itemLink},
+			Description: itemDescription,
+			Author:      &feeds.Author{Name: itemAuthor},
+			Created:     created,
+		})
+	}
 
-    return
+	return
 }
